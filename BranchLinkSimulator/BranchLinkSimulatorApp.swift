742caea9920c70145d1bdc11969314c306c1db4d
nsingh-branch <nipun.singh@branch.io>
2024-02-08 15:47:44 -0800

    Initial Commit

---
+++ BranchLinkSimulator/BranchLinkSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchLinkSimulatorApp.swift
+//  BranchLinkSimulator
+//
+//  Created by Nipun Singh on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BranchLinkSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
